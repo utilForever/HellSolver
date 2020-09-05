f89fef2f9d4c868d3e2ef4f11eafaafdfbffb136
Chris Ohk <utilforever@gmail.com>
2020-09-05 22:00:54 +0900

    feat(add-ci): Add header generator script

---
+++ Includes/HellSolver/HellSolver.hpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2020 HellSolver Team
+// Chris Ohk, Juhan Cha, Woosung Joung, Yongwook Choi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef HELL_SOLVER_HPP
+#define HELL_SOLVER_HPP
+
+#include <HellSolver/HellSolver.hpp>
+#include <HellSolver/Test.hpp>
+
+#endif  // HELL_SOLVER_HPP
--- Sources/HellSolver/Test.cpp
+++ Sources/HellSolver/Test.cpp
@@ -1,4 +1,4 @@
-#include <Test.hpp>
+#include <HellSolver/Test.hpp>
 
 int Add(int a, int b)
 {
