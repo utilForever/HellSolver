63bbe356072fed726fa2b6ced1650a8c5dce1f0b
hanchaa <hanchaa@gmail.com>
2020-09-07 12:33:22 +0900

    feat(enums): Add player status and direction enum

---
+++ Includes/HellSolver/Enums/GameEnums.hpp
@@ -0,0 +1,23 @@
+#ifndef HELL_SOLVER_GAME_ENUMS_HPP
+#define HELL_SOLVER_GAME_ENUMS_HPP
+
+namesapce hell_solver
+{
+enum class PlayerStatus
+{
+   PLAYING,
+   WIN,
+   LOST
+};
+
+enum class Direction
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
+
+}
+
+#endif  // !HELL_SOLVER_GAME_ENUMS_HPP
