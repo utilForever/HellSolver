9193ca9a921262051f8e65fb6dfd112a0442c294
hyperflow <hyperflow@kakao.com>
2020-09-09 00:31:15 +0900

    refactor: Add some header for copyright

--- Includes/HellSolver/Enums/GameEnums.hpp
+++ Includes/HellSolver/Enums/GameEnums.hpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2020 HellSolver Team
+// Chris Ohk, Juhan Cha, Woosung Joung, Yongwook Choi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
 #ifndef HELL_SOLVER_GAME_ENUMS_HPP
 #define HELL_SOLVER_GAME_ENUMS_HPP
 
--- Includes/HellSolver/Player/Player.hpp
+++ Includes/HellSolver/Player/Player.hpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2020 HellSolver Team
+// Chris Ohk, Juhan Cha, Woosung Joung, Yongwook Choi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
 #ifndef HELL_SOLVER_PLAYER_HPP
 #define HELL_SOLVER_PLAYER_HPP
 
