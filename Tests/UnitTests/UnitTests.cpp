dca6a44e88b1cd5137830cc2d251ca16f185c0bf
hyperflow <hyperflow@kakao.com>
2020-09-08 19:56:57 +0900

    test: Modify test for MoveState check

--- Tests/UnitTests/UnitTests.cpp
+++ Tests/UnitTests/UnitTests.cpp
@@ -10,12 +10,12 @@ using namespace hell_solver;
 
 TEST_CASE("MAP 1 - LOADING")
 {
-Game game(MAPS_DIR "1.txt");
+    Game game(MAPS_DIR "1.txt");
 
-CHECK(game.GetMap().At(1, 1).HasType(ObjectType::WALL));
-CHECK(game.GetMap().At(1, 6).HasType(ObjectType::PLAYER));
-CHECK(game.GetMap().At(2, 4).HasType(ObjectType::UNDEAD));
-CHECK(game.GetMap().At(5, 2).HasType(ObjectType::ROCK));
+    CHECK(game.GetMap().At(1, 1).HasType(ObjectType::WALL));
+    CHECK(game.GetMap().At(1, 6).HasType(ObjectType::PLAYER));
+    CHECK(game.GetMap().At(2, 4).HasType(ObjectType::UNDEAD));
+    CHECK(game.GetMap().At(5, 2).HasType(ObjectType::ROCK));
 }
 
 TEST_CASE("TEST - CanMoveTestMap")
@@ -24,5 +24,8 @@ TEST_CASE("TEST - CanMoveTestMap")
 
     CHECK(game.GetMap().At(2, 2).HasType(ObjectType::PLAYER));
     CHECK(game.GetMap().At(2, 3).HasType(ObjectType::EMPTY));
-    CHECK(game.CanMove(Direction::UP) == MoveState::MOVE);
+    CHECK(game.CanMove(Direction::UP) == MoveState::STOP);
+    CHECK(game.CanMove(Direction::LEFT) == MoveState::STOP);
+    CHECK(game.CanMove(Direction::DOWN) == MoveState::MOVE);
+    CHECK(game.CanMove(Direction::RIGHT) == MoveState::MOVE);
 }
