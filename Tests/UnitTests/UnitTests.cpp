b4315524e963bd0c1d14a0ea0c40f4d8acfcabfd
hyperflow <hyperflow@kakao.com>
2020-09-08 21:57:01 +0900

    test: Add test for spike and game ending

--- Tests/UnitTests/UnitTests.cpp
+++ Tests/UnitTests/UnitTests.cpp
@@ -3,6 +3,7 @@
 //
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
+#include <iostream>
 
 #include <HellSolver/Games/Game.hpp>
 
@@ -28,4 +29,19 @@ TEST_CASE("TEST - CanMoveTestMap")
     CHECK(game.CanMove(Direction::LEFT) == MoveState::STOP);
     CHECK(game.CanMove(Direction::DOWN) == MoveState::MOVE);
     CHECK(game.CanMove(Direction::RIGHT) == MoveState::MOVE);
+}
+
+TEST_CASE("TEST - IsGameEndTest")
+{
+    Game game(TEST_MAPS_DIR "IsGameEndTest.txt");
+
+    CHECK(game.GetMap().At(1, 1).HasType(ObjectType::PLAYER));
+    CHECK(game.GetPlayer().GetMoveCount() == 10);
+    CHECK(game.CanMove(Direction::UP) == MoveState::STOP);
+    CHECK(game.MovePlayer(Direction::UP) == PlayerStatus::PLAYING);
+    CHECK(game.GetPlayer().GetMoveCount() == 10);
+    CHECK(game.CanMove(Direction::RIGHT) == MoveState::MOVE);
+    CHECK(game.MovePlayer(Direction::RIGHT) == PlayerStatus::PLAYING);
+    CHECK(game.GetPlayer().GetMoveCount() == 8);
+    CHECK(game.MovePlayer(Direction::RIGHT) == PlayerStatus::WIN);
 }
